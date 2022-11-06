a14b2a8a595b03d2d4314fb95b3ece055bd864a8
Akuta_Zehy <akuta_zehy@outlook.com>
2022-11-06 14:42:43 +0800

    Mybatis config

---
+++ ssm_integration/src/main/java/com/akutazehy/ssm/mapper/EmployeeMapper.java
@@ -0,0 +1,9 @@
+package com.akutazehy.ssm.mapper;
+
+/**
+ * @author Akuta_Zehy
+ * @version 1.0
+ */
+public interface EmployeeMapper {
+
+}
--- ssm_integration/src/main/java/com/akutazehy/ssm/service/impl/EmployeeServiceImpl.java
+++ ssm_integration/src/main/java/com/akutazehy/ssm/service/impl/EmployeeServiceImpl.java
@@ -1,6 +1,8 @@
 package com.akutazehy.ssm.service.impl;
 
 import com.akutazehy.ssm.service.EmployeeService;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
