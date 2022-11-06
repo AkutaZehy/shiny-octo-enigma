1e0211748088f2734597a150f8b5211e3f28f6db
Akuta Zehy <18171453966@163.com>
2022-11-06 16:18:39 +0800

    建表

---
+++ CREATE TABLE.sql
@@ -0,0 +1,10 @@
+`t_emp`-- auto-generated definition
+CREATE TABLE t_emp
+(
+    emp_id   INT AUTO_INCREMENT
+        PRIMARY KEY,
+    emp_name VARCHAR(20) NULL,
+    age      INT         NULL,
+    gender   CHAR        NULL,
+    email VARCHAR(50)         NULL
+)COMMENT '员工表' AUTO_INCREMENT = 34;
