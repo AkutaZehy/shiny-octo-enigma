2b37e79515489ce03e3e58a78c93261237dbb581
Akuta_Zehy <akuta_zehy@outlook.com>
2022-11-06 12:37:45 +0800

    SpringMVC config

---
+++ ssm_integration/src/main/java/com/akutazehy/ssm/controller/EmployController.java
@@ -0,0 +1,12 @@
+package com.akutazehy.ssm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Akuta_Zehy
+ * @version 1.0
+ */
+
+@Controller
+public class EmployController {}
